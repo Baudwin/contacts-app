b8c7e5161dd341c0802212a375dda1f68a293df0
Titah Baudwin <baudwin12@gmail.com>
2023-08-01 00:41:26 +0100

    error message for incorrect cridentials added to login page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const ejs  = require("ejs")
+const ejs = require("ejs")
 const mysql = require("mysql2")
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const app = express()
-app.set("view engine",  "ejs")
-app.use(bodyParser.urlencoded({extended:true}))
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/public"))
 
 
@@ -31,31 +31,31 @@ database.connect((err) => {
 
 
 
-app.get("/" , (req,res)=>{
+app.get("/", (req, res) => {
     res.render("login")
 })
 
-app.get("/signup" , (req,res)=>{
+app.get("/signup", (req, res) => {
     res.render("signup")
 })
 
 
-app.post("/signup", async(req,res)=>{
+app.post("/signup", async (req, res) => {
     let username = req.body.username
     let email = req.body.email
     let password = req.body.password
 
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+
         let command = `insert into user
         values(userID,"${username}","${hash}","${email}")`
 
-         database.query( command,(err,result)=>{
-        if (err) {
-           return res.render("signup")
-        }
-        res.redirect("/")
-    })
+        database.query(command, (err, result) => {
+            if (err) {
+                return res.render("signup")
+            }
+            res.redirect("/")
+        })
 
     });
 
@@ -65,30 +65,34 @@ app.post("/signup", async(req,res)=>{
 })
 
 
-app.post("/login", (req,res)=>{
+app.post("/login", (req, res) => {
     let username = req.body.username
     let password = req.body.password
     let command = `select * from user
     WHERE username = '${username}'`
-    database.query(command, (err,users)=>{
+    database.query(command, (err, users) => {
 
 
         if (users.length < 1) {
             let errmsg = "Incorrect Username or Password"
-           return res.render("login", {errmsg:errmsg})
+            return res.render("login", { errmsg: errmsg })
         }
 
         if (users.length > 0) {
-            
-        users.forEach(user => {
 
-            bcrypt.compare(password, user.password, function(err, result) {
-               if (result === true) {
-              res.redirect(`/contacts/${user.userID}`)
-                
-               }
+            users.forEach(user => {
+
+                bcrypt.compare(password, user.password, function (err, result) {
+                    if (result === true) {
+                        return res.redirect(`/contacts/${user.userID}`)
+
+                    } 
+                    else {
+                        let errmsg = "Incorrect Username or Password"
+                        return res.render("login", { errmsg: errmsg })
+                    }
+                });
             });
-        });
         }
 
 
@@ -100,7 +104,7 @@ app.post("/login", (req,res)=>{
 
 
 
-app.get("/contacts/:userID", (req,res)=>{
+app.get("/contacts/:userID", (req, res) => {
     let userid = req.params.userID
 
     let command = `select * from contact
@@ -108,15 +112,15 @@ app.get("/contacts/:userID", (req,res)=>{
     where user = ${userid}
     ORDER BY firstName ASC`
 
-    
+
     let command2 = `select * from user
     where userID = ${userid}`
-    
 
-    database.query(command, (err,result)=>{
-        database.query(command2, (err,user)=>{
-                res.render("contacts", {contact:result, user:user})
-  
+
+    database.query(command, (err, result) => {
+        database.query(command2, (err, user) => {
+            res.render("contacts", { contact: result, user: user })
+
         })
     })
 
@@ -125,12 +129,12 @@ app.get("/contacts/:userID", (req,res)=>{
 
 
 
-app.get("/new-contact/:userID", (req,res)=>{
-    let userId  = req.params.userID;
-    res.render("new-contact", {userID:userId})
+app.get("/new-contact/:userID", (req, res) => {
+    let userId = req.params.userID;
+    res.render("new-contact", { userID: userId })
 })
 
-app.post("/add-contact",(req,res)=>{
+app.post("/add-contact", (req, res) => {
     let user = req.body.user
     let fname = req.body.fName
     let mname = req.body.mName
@@ -138,12 +142,12 @@ app.post("/add-contact",(req,res)=>{
     let number = req.body.number
     let email = req.body.email
     let address = req.body.address
-    let command  = `insert into Contact 
+    let command = `insert into Contact 
     values(contactID,${user} ,'${fname}', '${mname}','${lname}','${number}','${address}', '${email}', current_date(), current_time())`
-    database.query(command, (err,result)=>{
+    database.query(command, (err, result) => {
         if (err) {
             console.log(err);
-        }else{
+        } else {
             res.redirect(`/contacts/${user}`)
         }
     })
@@ -152,40 +156,40 @@ app.post("/add-contact",(req,res)=>{
 
 
 
-app.get("/contact-info/:contactID", (req,res)=>{
+app.get("/contact-info/:contactID", (req, res) => {
     let id = req.params.contactID
     let command = `select * from Contact
     WHERE contactID = ${id}`
-    database.query(command, (err,result)=>{
+    database.query(command, (err, result) => {
         if (err) {
             console.log(err);
         }
 
         result.forEach(result => {
-             res.render("contact-info", {contact:result})   
+            res.render("contact-info", { contact: result })
         });
     })
 
 })
 
 
-app.get("/edit-contact/:contactId", (req,res)=>{
+app.get("/edit-contact/:contactId", (req, res) => {
     let id = req.params.contactId
-   let command = `select * from Contact
+    let command = `select * from Contact
    WHERE contactID = ${id}`
 
-   let command2 = `select user from Contact
+    let command2 = `select user from Contact
    WHERE contactID = ${id}`
 
-   database.query(command , (err,result)=>{
-    database.query(command2, (err,user)=>{
+    database.query(command, (err, result) => {
+        database.query(command2, (err, user) => {
 
-        res.render("edit-contact",{contact:result})
+            res.render("edit-contact", { contact: result })
+        })
     })
-   })
 })
 
-app.post("/update-contact", (req,res)=>{
+app.post("/update-contact", (req, res) => {
     let id = req.body.updatebtn
     let fname = req.body.fName
     let mname = req.body.mName
@@ -193,14 +197,14 @@ app.post("/update-contact", (req,res)=>{
     let number = req.body.number
     let email = req.body.email
     let address = req.body.address
-    let command  = `update Contact
+    let command = `update Contact
     SET firstName = "${fname}", middleName = "${mname}", lastName = "${lname}", 
     phoneNumber = "${number}", email = "${email}", address = "${address}"
     WHERE contactID = ${id} `
-    database.query(command, (err,result)=>{
+    database.query(command, (err, result) => {
         if (err) {
             console.log(err);
-        }else{
+        } else {
             res.redirect(`/contact-info/${id}`)
         }
     })
@@ -208,12 +212,12 @@ app.post("/update-contact", (req,res)=>{
 
 
 
-app.post("/delete", (req,res)=>{
+app.post("/delete", (req, res) => {
     let id = req.body.deletebtn
     let userID = req.body.userID
     let command = `delete from Contact
     WHERE contactID = ${id}`
-    database.query(command, (err,result)=>{
+    database.query(command, (err, result) => {
         if (err) {
             console.log(err);
         }
@@ -229,6 +233,6 @@ app.post("/delete", (req,res)=>{
 
 
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("Server started on port 4000");
 })
